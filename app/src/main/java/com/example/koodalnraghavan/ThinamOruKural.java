a12764217de2e94525f96fb4dd7cdbbb5e0e00ad
sunilkumar001 <sunilselva3335@gmail.com>
2020-04-24 17:21:18 +0530

    sixth commit

--- app/src/main/java/com/example/koodalnraghavan/ThinamOruKural.java
+++ app/src/main/java/com/example/koodalnraghavan/ThinamOruKural.java
@@ -1,15 +1,27 @@
 package com.example.koodalnraghavan;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
-public class ThinamOruKural extends AppCompatActivity {
+public class ThinamOruKural extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -18,6 +30,7 @@ public class ThinamOruKural extends AppCompatActivity {
 
     public PageAdapter pageAdapter;
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +40,18 @@ public class ThinamOruKural extends AppCompatActivity {
         MusicTab = findViewById(R.id.musictab);
         VideoTab = findViewById(R.id.videotab);
         viewPager = findViewById(R.id.viewpager);
+
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigationbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
         pageAdapter = new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
         viewPager.setAdapter(pageAdapter);
@@ -58,5 +83,10 @@ public class ThinamOruKural extends AppCompatActivity {
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
