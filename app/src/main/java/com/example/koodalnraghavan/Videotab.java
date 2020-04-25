58d206cfeff16748623eef2d66d95ec298797b54
sunilkumar001 <sunilselva3335@gmail.com>
2020-04-25 22:09:54 +0530

    seventh commit

--- app/src/main/java/com/example/koodalnraghavan/Videotab.java
+++ app/src/main/java/com/example/koodalnraghavan/Videotab.java
@@ -1,8 +1,11 @@
 package com.example.koodalnraghavan;
 
 import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,9 +16,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +31,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
 
 public class Videotab extends Fragment {
 
@@ -33,6 +40,8 @@ public class Videotab extends Fragment {
     private ProgressBar progressBar;
     private ImageView Stop;
     private ImageView Pause;
+    private Button Download;
+    private TextView PlayingSong;
 
     private ArrayList<String> list;
     private ArrayList<String> url;
@@ -57,11 +66,17 @@ public class Videotab extends Fragment {
         progressBar = view.findViewById(R.id.progress);
         Stop = view.findViewById(R.id.stop);
         Pause = view.findViewById(R.id.pause);
+        Download = view.findViewById(R.id.download);
+        PlayingSong = view.findViewById(R.id.playinginfo);
+
+        PlayingSong.setSelected(true);
 
         database = FirebaseDatabase.getInstance();
         reference = database.getReference("DailyMusic");
         list = new ArrayList<String>();
         url = new ArrayList<String>();
+
+        mediaPlayer = new MediaPlayer();
 
         pausePlayCounter =0;
 
@@ -80,14 +95,14 @@ public class Videotab extends Fragment {
                 }
                 progressBar.setVisibility(View.GONE);
                 listView.setAdapter(adapter);
-                mediaPlayer = new MediaPlayer();
+
                 mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
                 {
                     @Override
-                    public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                    public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
 
-                        String portalUrl = url.get(position);
+                        final String portalUrl = url.get(position);
 
                         try {
                             mediaPlayer.stop();
@@ -97,6 +112,9 @@ public class Videotab extends Fragment {
                             Toast.makeText(view.getContext(),"Play",Toast.LENGTH_SHORT).show();
                             mediaPlayer.start();
                             pausePlayCounter=0;
+
+                            PlayingSong.setText("Playing now : " + list.get(position));
+                            PlayingSong.setSelected(true);
                         }
                         catch (Exception e)
                         {
@@ -125,7 +143,34 @@ public class Videotab extends Fragment {
                             @Override
                             public void onClick(View v) {
                                 mediaPlayer.stop();
+
+                                PlayingSong.setText("Stopped : " + list.get(position));
+                                PlayingSong.setSelected(true);
                                 return;
+                            }
+                        });
+
+                        Download.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                Toast.makeText(getContext(),"Downloading : " + list.get(position),Toast.LENGTH_SHORT).show();
+
+                                Uri uri = Uri.parse(portalUrl);
+
+                                DownloadManager downloadManager = (DownloadManager) view.getContext().getSystemService(Context.DOWNLOAD_SERVICE);
+                                DownloadManager.Request request = new DownloadManager.Request(uri);
+
+                                Context context = view.getContext();
+                                String fileename = list.get(position);
+                                String fileExtension = ".mp3";
+
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                request.setDestinationInExternalFilesDir(context,DIRECTORY_DOWNLOADS,fileename + fileExtension);
+
+                                downloadManager.enqueue(request);
+
                             }
                         });
                     }
@@ -137,6 +182,8 @@ public class Videotab extends Fragment {
 
             }
         });
+
+        mediaPlayer.stop();
         return view;
     }
 }
