b65a6d45b66f3499b50612d6080596867d05e1ae
THIRUYOGESH <thiruyogesh944@gmail.com>
2020-04-26 18:26:02 +0530

    yogesh

---
+++ app/src/main/java/com/example/koodalnraghavan/jodhidam.java
@@ -0,0 +1,78 @@
+package com.example.koodalnraghavan;
+
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.koodalnraghavan.models.MediaObject;
+import com.example.koodalnraghavan.util.Resources;
+import com.example.koodalnraghavan.util.VerticalSpacingItemDecorator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+public class jodhidam extends AppCompatActivity {
+
+    private static final String TAG = "jodhidam";
+
+    private VideoPlayerRecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jodhidam);
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
+
+        ArrayList<MediaObject> mediaObjects = new ArrayList<MediaObject>(Arrays.asList(Resources.MEDIA_OBJECTS));
+        mRecyclerView.setMediaObjects(mediaObjects);
+        VideoPlayerRecyclerAdapter adapter = new VideoPlayerRecyclerAdapter(mediaObjects, initGlide());
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    private RequestManager initGlide(){
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.white_background)
+                .error(R.drawable.white_background);
+
+        return Glide.with(this)
+                .setDefaultRequestOptions(options);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        if(mRecyclerView!=null)
+            mRecyclerView.releasePlayer();
+        super.onDestroy();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
